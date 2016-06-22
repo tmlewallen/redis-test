01a744d3f17de99da01643ef90a0944baed55e02
tmlewallen <tmlewallen@crimson.ua.edu>
2016-06-22 18:10:53 -0400

    initial commit :sparkles:

---
+++ src/main/java/com/example/RedisTestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisTestApplication.class, args);
+	}
+}
